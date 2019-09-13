74f552f26d3fe248e90a3321b05e228f437de546
Andreas Longva <andreas.b.longva@gmail.com>
2019-09-13 10:36:46 +0200

    Initial prototype

---
+++ build.rs
@@ -0,0 +1,17 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    pkg_config::probe_library("mkl-dynamic-lp64-seq").unwrap();
+
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
